87c43a5361697403d6dccaa157199d6bfb8a0c13
Andrea Finch <aefinch@vt.edu>
2017-04-06 10:17:45 -0500

    completed part 4.2

--- songs.js
+++ songs.js
@@ -2,22 +2,37 @@ var songs = [];
 var deleteBtn = [];
 var button = document.getElementById("submitSong");
 var songRequest = new XMLHttpRequest();
-
+var songRequest2 = new XMLHttpRequest();
 var playList = document.getElementById("songList");
+var moreMusic;
 
 function removeSong(){
 	var songIndex = deleteBtn.indexOf(event.target);
-	console.log(songs);
 	songs.splice(songIndex, 1);
-	console.log(songs);
 	printPlayList(songs);
 };
 
+function addToSongs(){
+	var data2 = JSON.parse(this.responseText);	
+	songs = songs.concat(data2.songs);
+	printPlayList(songs);
+};
+
+function loadSideB(){
+	songRequest2.addEventListener("load", addToSongs);
+	songRequest2.open("GET", "sideB.json");
+	songRequest2.send();
+	moreMusic.setAttribute("disabled", "disabled");
+};
+
 function activateEvents(listLength){
+	console.log(listLength);
 	for (var j = 0; j< listLength; j++){
 		deleteBtn[j] = document.getElementById(`delete${j}`);
 		deleteBtn[j].addEventListener("click", removeSong); 
 	}
+	moreMusic = document.getElementById("more");
+	moreMusic.addEventListener("click", loadSideB);
 }
 function printPlayList(songData){
 	playList.innerHTML = "";
@@ -29,6 +44,7 @@ function printPlayList(songData){
 		var genre = currentSong.genre;
 		playList.innerHTML += `<h3>${fullSongName}</h3><ul><li class = 'songInfo'>${fullArtist}</li><li class = 'songInfo'>|</li><li class = 'songInfo'>${fullAlbum}</li><li class = 'songInfo'>|</li><li class = 'songInfo'>${genre}</li><li class = 'songInfo'>|</li><li class = 'songInfo'><button id = "delete${i}">Delete</button></li> </ul>`;
 	}
+		// playList.innerHTML += `<br><br><button id="more">More</button>`;
 
 		activateEvents(i);	
 }
